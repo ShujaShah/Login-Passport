36748984fd267d4491843f07c8d621d3003407d1
shuja-shah3 <shuja.shah@iquasar.com>
2023-11-27 12:41:34 +0530

    changed the cluster

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 dbPassword =
-  "mongodb+srv://shuja:" +
-  encodeURIComponent("shuja") +
-  "@cluster0.ndy1scg.mongodb.net/?retryWrites=true&w=majority";
+  'mongodb+srv://shuja:' +
+  encodeURIComponent('shuja') +
+  '@cluster0.bugz1bo.mongodb.net/?retryWrites=true&w=majority';
 
 module.exports = {
   mongoURI: dbPassword,

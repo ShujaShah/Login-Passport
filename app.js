e25711da4daa035d9662d59c68568fdb7e294350
shuja-shah3 <shuja.shah@iquasar.com>
2023-11-27 16:58:27 +0530

    added env

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const mongoose = require("mongoose");
-const passport = require("passport");
-const flash = require("connect-flash");
-const session = require("express-session");
-const TodoTask = require("./models/entity/ToDoTask");
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+const TodoTask = require('./models/entity/ToDoTask');
+const dotenv = require('dotenv');
 
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
 //use the stylesheets
 
-app.use("/static", express.static("public"));
+app.use('/static', express.static('public'));
 
 // Passport Config
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 // // DB Config
-const db = require("./config/keys").mongoURI;
-mongoose.set("useFindAndModify", false);
+const db = require('./config/keys').mongoURI;
+mongoose.set('useFindAndModify', false);
 
 // Connect to MongoDB
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB Connected"))
+  .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.log(err));
 
 const connection = mongoose.createConnection(db, {
@@ -32,24 +33,24 @@ const connection = mongoose.createConnection(db, {
   useUnifiedTopology: true,
 });
 
-const MongoStore = require("connect-mongo")(session);
+const MongoStore = require('connect-mongo')(session);
 
 // EJS
 app.use(expressLayouts);
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // Express body parser
 app.use(express.urlencoded({ extended: true }));
 
 const sessionStore = new MongoStore({
   mongooseConnection: connection,
-  collection: "sessionStore",
+  collection: 'sessionStore',
 });
 
 // Express session
 app.use(
   session({
-    secret: "secret",
+    secret: 'secret',
     resave: true,
     saveUninitialized: true,
     store: sessionStore,
@@ -68,16 +69,16 @@ app.use(flash());
 
 // Global variables for displaying the flash messages
 app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash("success_msg");
-  res.locals.error_msg = req.flash("error_msg");
-  res.locals.error = req.flash("error");
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
   next();
 });
 
 // Routes
-app.use("/", require("./routes/index.js"));
-app.use("/users", require("./routes/users.js"));
-app.use("/auth", require("./routes/oauth.js"));
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+app.use('/auth', require('./routes/oauth.js'));
 
 //
 passport.serializeUser(function (user_id, done) {
@@ -86,5 +87,5 @@ passport.serializeUser(function (user_id, done) {
 passport.deserializeUser(function (user_id, done) {
   done(null, user_id);
 });
-
-app.listen(3000, console.log(`Server running on 3000`));
+const port = process.env.PORT || 3000;
+app.listen(port, console.log(`Server running on ${port}`));
